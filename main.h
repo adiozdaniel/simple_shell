e38bba0f9664e6da54e4dea448df606ac63f7170
Happy Chukwuma <happychukwuma@gmail.com>
2023-11-09 03:52:29 -0800

    Added more functionalities - _memory.c

--- main.h
+++ main.h
@@ -171,6 +171,11 @@ void bring_line(char **lineptr, size_t *n, char *buffer, size_t j);
 ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
 int exec_line(data_shell *datash);
 
+/* _memory.c */
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
+
 /* _sigint */
 void get_sigint(int sig);
 
